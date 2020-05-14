8c414bc75487d5ea3f7e1806c6940b4037916ec7
fkeck <francois.keck@gmail.com>
2020-05-14 23:27:46 +0200

    Add tests for consensus ambiguity

--- tests/testthat/test-op-summarize.R
+++ tests/testthat/test-op-summarize.R
@@ -40,3 +40,72 @@ test_that("Clustering fails if different length sequences", {
   expect_error(seq_cluster(x), "length")
 })
 
+
+
+
+test_that("Consensus majority works with DNA", {
+  x <- dna("-----TACGCAGTAAAAGCTACTGATG",
+           "CGTCATACGCAGTAAAAACTACTGATG",
+           "CTTCATACGCAGTAAAAACTACTGATG",
+           "CTTCATATGCAGTAAAATCTACTGATG",
+           "CTTCATACGCAGTAAAAACTACTGATG",
+           "CGTCATACGCAGTAAAAGCTACTGATG",
+           "CTTCATATGCAGTAAAAGCTACTGACG")
+  cons <- seq_consensus(x, method = "chr_majority")
+  expect_equal(cons, dna("CTTCATACGCAGTAAAARCTACTGATG"))
+  cons <- seq_consensus(x, method = "chr_majority",
+                        weights = c(10, 1, 1, 1, 1, 1, 1))
+  expect_equal(cons, dna("-----TACGCAGTAAAAGCTACTGATG"))
+})
+
+test_that("Consensus majority works with DNA", {
+  x <- rna("-----UACGCAGUAAAAGCUACUGAUG",
+           "CGUCAUACGCAGUAAAAACUACUGAUG",
+           "CUUCAUACGCAGUAAAAACUACUGAUG",
+           "CUUCAUAUGCAGUAAAAUCUACUGAUG",
+           "CUUCAUACGCAGUAAAAACUACUGAUG",
+           "CGUCAUACGCAGUAAAAGCUACUGAUG",
+           "CUUCAUAUGCAGUAAAAGCUACUGACG")
+  cons <- seq_consensus(x, method = "chr_majority")
+  expect_equal(cons, rna("CUUCAUACGCAGUAAAARCUACUGAUG"))
+})
+
+test_that("Consensus majority works with AA", {
+  x <-  aa("CKLSI", "CLLSL")
+  cons <- seq_consensus(x, method = "chr_majority")
+  expect_equal(cons, aa("CXLSJ"))
+})
+
+
+test_that("Consensus ambiguity works with DNA", {
+  x <- dna("-----TACGCAGTAAAAGCTACTGATG",
+           "CGTCATACGCAGTAAAAACTACTGATG",
+           "CTTCATACGCAGTAAAAACTACTGATG",
+           "CTTCATATGCAGTAAAATCTACTGATG",
+           "CTTCATACGCAGTAAAAACTACTGATG",
+           "CGTCATACGCAGTAAAAGCTACTGATG",
+           "CTTCATATGCAGTAAAAGCTACTGACG")
+  cons <- seq_consensus(x, method = "chr_ambiguity")
+  expect_equal(cons, dna("-----TAYGCAGTAAAADCTACTGAYG"))
+  cons <- seq_consensus(x, method = "chr_ambiguity",
+                        weights = c(10, 1, 1, 1, 1, 1, 1))
+  expect_equal(cons, dna("-----TAYGCAGTAAAADCTACTGAYG"))
+})
+
+test_that("Consensus majority works with DNA", {
+  x <- rna("-----UACGCAGUAAAAGCUACUGAUG",
+           "CGUCAUACGCAGUAAAAACUACUGAUG",
+           "CUUCAUACGCAGUAAAAACUACUGAUG",
+           "CUUCAUAUGCAGUAAAAUCUACUGAUG",
+           "CUUCAUACGCAGUAAAAACUACUGAUG",
+           "CGUCAUACGCAGUAAAAGCUACUGAUG",
+           "CUUCAUAUGCAGUAAAAGCUACUGACG")
+  cons <- seq_consensus(x, method = "chr_ambiguity")
+  expect_equal(cons, rna("-----UAYGCAGUAAAADCUACUGAYG"))
+})
+
+test_that("Consensus majority works with AA", {
+  x <-  aa("CKLSI", "CKLSI", "CLLSL")
+  cons <- seq_consensus(x, method = "chr_ambiguity")
+  expect_equal(cons, aa("CXLSJ"))
+})
