9365bb8c912c48753dda895149abb85b7d499370
fkeck <francois.keck@gmail.com>
2020-11-25 16:48:35 +0100

    keep only S3 regitration code

--- R/zzz.R
+++ R/zzz.R
@@ -3,12 +3,5 @@
   vctrs::s3_register("pillar::pillar_shaft", "bioseq_dna")
   vctrs::s3_register("pillar::pillar_shaft", "bioseq_rna")
   vctrs::s3_register("pillar::pillar_shaft", "bioseq_aa")
-
-  if (utils::packageVersion("vctrs") <= "0.1.0") {
-    vec_default_cast <<- vec_default_cast_old
-  } else {
-    rm("vec_default_cast", inherits = TRUE)
-  }
-
   invisible()
 }
