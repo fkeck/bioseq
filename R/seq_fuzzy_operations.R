1c0c167bc6547e38e69e46800d6055bef2621aa2
fkeck <francois.keck@gmail.com>
2021-02-10 21:20:18 +0100

    delete old doc

--- R/seq_fuzzy_operations.R
+++ R/seq_fuzzy_operations.R
@@ -30,69 +30,15 @@ seq_detect_fuzzypattern <- function(x, pattern, max_dist = 0.0) {
 
 
 
-#' Crop sequences using fuzzy delimiting patterns
-#'
-#' This function extracts sub-sequences given two
-#' patterns delimiting the cropping region.
-#' This is useful to isolate a genetic region
-#' from two sets of primers (like a PCR would do).
-#' The function uses approximate string matching,
-#' which allows to specify an error rate for pattern matching.
-#'
-#' @param x a DNA, RNA or AA vector to be cropped.
-#' @param pattern_in a vector of patterns to be matched
-#' against the sequences in \code{x}
-#' and defining the beginning (left-side) of the returned
-#' sequences. Pass \code{NULL} to avoid cropping.
-#' @param pattern_out a vector of patterns to be matched
-#' against the sequences in \code{x}
-#' and defining the end (right-side) of the returned sequences.
-#' Pass \code{NULL} to avoid cropping.
-#' @param max_dist_in the maximum edit distance between the
-#' sequence and the best matching \code{pattern_in}
-#' i.e. maximum error allowed between the sequence and the patterns.
-#' If the error rate is higher,
-#' this will produce \code{NA}.
-#' @param max_dist_out same for \code{pattern_out}.
-#' @param include_patterns A logical. Should the patterns included
-#' in the returned cropped sequences (default is \code{TRUE}).
-#'
-#' @details
-#'
-#' If \code{pattern_in}/\code{pattern_out} are DNA, RNA or AA vectors,
-#' patterns sequences will be processed with \code{seq_disambiguate_IUPAC}.
-#'
-#' Currently, ambiguities in the input sequences are ignored.
-#'
-#' @inheritSection seq_detect_pattern Patterns
-#'
-#' @return A character vector of cropped sequences.
-#'
-#' @examples
-#' x <-as_dna(c("ACTGGTCAGCATTAGTGCTACTGTTACAGGT", "GCCCCTGCAATT", "AAAAGGGTTTTTTT"))
-#'
-#'
-#' # If no match result will be NA
-#' seq_crop_pattern(x, pattern_in = dna("TCAGCATT", "CCCCTG"),
-#'                   pattern_out = dna("TGTTACA"))
-#'
-#' # Pattern can be a regex.
-#' #seq_crop_pattern(x, pattern_in = "A..A..A")
-#'
-#' # Ambiguous patterns
-#' x <- dna("ACGATTTTTTT")
-#' seq_crop_pattern(x, pattern_in = dna("ACRA"),
-#'                 include_patterns = FALSE)
-#'
-#' x <- dna("ACGAGTTTTTTCCCCCCC", "VACGAGTTTTTTCCCCCCC")
-#' seq_crop_fuzzypattern(x,
-#'                  pattern_in = dna("ACRAN"),
-#'                  pattern_out = dna("CCCC", "GGGG"),
-#'                  max_dist_in = 0,
-#'                  max_dist_out = 0,
-#'                  include_patterns = FALSE)
-#'
-#'
+# Crop sequences using fuzzy delimiting patterns
+#
+# This function extracts sub-sequences given two
+# patterns delimiting the cropping region.
+# This is useful to isolate a genetic region
+# from two sets of primers (like a PCR would do).
+# The function uses approximate string matching,
+# which allows to specify an error rate for pattern matching.
+#
 seq_crop_fuzzypattern <- function(x,
                                   pattern_in = NULL,
                                   pattern_out = NULL,
